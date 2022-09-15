8ed948346d6085637e06b983d7efaf16917c20b7
AliceThunderWind <dalia.maillefer@gmail.com>
2022-09-15 12:56:55 +0200

    Updated GE, VD, FR

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="text-center mt-5">
       <h1>Carte des points d'intérêts de la Suisse Romande</h1>
-      <p class="lead">Mise à jour le 16 février 2022</p>
+      <p class="lead">Mise à jour le 15 septembre 2022 (seulement VD, GE, FR) </p>
       <p>Version v0.1.0</p>
     </div>
   </div>
