68c7ef998e3a2425d87559b09a117e91ec749019
AliceThunderWind <dalia.maillefer@gmail.com>
2022-10-30 12:22:12 +0100

    New pokestop in Vaud

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="text-center mt-5">
       <h1>Carte des points d'intérêts de la Suisse Romande</h1>
-      <p class="lead">Mise à jour le 19 septembre 2022</p>
+      <p class="lead">Mise à jour le 30 octobre 2022 (Canton de Vaud seulement) </p>
       <p>Version v0.1.0</p>
     </div>
   </div>
